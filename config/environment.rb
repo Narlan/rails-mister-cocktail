b00270651ed161c17194f847c2c734cd79b587ec
Adam Castro Guzman <castroguzman033@gmail.com>
2019-08-08 12:40:22 +0200

    Data base done

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-const { environment } = require('@rails/webpacker')
 
 # Initialize the Rails application.
 Rails.application.initialize!
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
-
-module.exports = environment
